d8aa063a152a7063c7f04669796599a1fc5b8d3a
beebozy <musahabeeblai@gmail.com.com>
2024-10-04 00:24:25 +0100

    Deployment added

--- ignition/modules/web3.ts
+++ ignition/modules/web3.ts
@@ -5,7 +5,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const  webModule = buildModule("webModule", (m) => {
  
 
-  const lock = m.contract("weIb3CXI")
+  const lock = m.contract("web3CXI")
 
   return { lock };
 });
